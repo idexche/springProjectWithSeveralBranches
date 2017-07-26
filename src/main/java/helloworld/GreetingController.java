415dfc94781368b886600ebfa982a092af2dc80d
idexche <usert2iov7@some.mail>
2017-07-26 00:30:01 +0000

    commitchk_remoteWed Jul 26 00:29:59 UTC 2017

--- src/main/java/helloworld/GreetingController.java
+++ src/main/java/helloworld/GreetingController.java
@@ -1,5 +1,5 @@
 package helloworld;
-//1500937147450
+//1501028850978
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,3 +1,3 @@
-<!1500937188387>
+<!1501028892409>
    response.sendRedirect("spring/hello");
 %>
