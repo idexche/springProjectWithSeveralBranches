45cba60acde3476f596910b725747950b8356f30
idexche <che@eclipse.org>
2017-10-23 18:40:56 +0000

    commitchk_remoteMon Oct 23 18:40:55 UTC 2017

--- src/main/java/helloworld/GreetingController.java
+++ src/main/java/helloworld/GreetingController.java
@@ -1,5 +1,5 @@
 package helloworld;
-//1508705529113
+//1508783920528
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,3 +1,3 @@
-<!1508705573440>
+<!1508783968077>
    response.sendRedirect("spring/hello");
 %>
